edf0c3a5ac424089205d948a7e3103dcba823da1
Weslley Lima <94873558+weslley-lima04@users.noreply.github.com>
2022-08-22 22:20:19 -0300

    adicionamos construtores
    aula 22.08

--- Conta.php
+++ Conta.php
@@ -7,7 +7,10 @@ class Contas
  var $numConta;
  var $nome;
  var $saldo;
-
+ var $dataDeCriacao;
+ var $senha;
+ var $saldo;
+ var $cancelada;
 
 
 	function ExibeDados()
@@ -18,6 +21,26 @@ class Contas
 		echo "Nome: ".$this->nome."<br/>"; 
 		echo "Conta: ".$this->saldo."<br/>"; 
 
+	}
+
+	function depositar($saldo)
+	{
+		this->saldo = $saldo;
+		echo "Seu saldo é ".{$this->saldo};
+	}
+
+
+	function __construct($ag, $saldo, $codigo, $dataCriacao, $titular, $senha)
+	{
+		$this->numAge = $ag;
+		$this->saldo = $saldo;
+		$this->numConta = $codigo;
+		$this->nome = $titular;
+		$this->dataDeCriacao = $dataCriacao;
+		$this->senha = $senha;
+
+		$this->depositar($saldo);
+		$this->cancelada = false;
 	}
 
 }
